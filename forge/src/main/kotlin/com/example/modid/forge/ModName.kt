f336eec67d0f94fbb987754feb8026de074508c5
lilgallon <lilian@gallon.dev>
2023-05-29 08:36:42 +0200

    (forge): added example code to init config screen

--- forge/src/main/kotlin/com/example/modid/forge/ModName.kt
+++ forge/src/main/kotlin/com/example/modid/forge/ModName.kt
@@ -3,23 +3,30 @@ package com.example.modid.forge
 import com.example.modid.forge.config.ModConfig
 import me.shedaniel.autoconfig.AutoConfig
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.screens.Screen
+import net.minecraftforge.client.ConfigScreenHandler
+import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 
 @Mod("modid")
 class ModName {
     init {
-        // TODO: fix: screen is not accessible from forge mod menu
+        // Init config screen
         AutoConfig.register(ModConfig::class.java, ::JanksonConfigSerializer)
+        ModLoadingContext
+            .get()
+            .registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory::class.java) {
+                ConfigScreenHandler.ConfigScreenFactory { _: Minecraft?, parent: Screen? ->
+                    AutoConfig.getConfigScreen(
+                        ModConfig::class.java,
+                        parent,
+                    ).get()
+                }
+            }
+
+        // Use config
         val config = AutoConfig.getConfigHolder(ModConfig::class.java).config
-
-        // TODO: finish this
-//        ModLoadingContext
-//            .get()
-//            .registerExtensionPoint(
-//                ConfigScreenFactory::class.java,
-//                () -> (client, parent) -> AutoConfig.getConfigScreen(ModConfig::class.java, parent).get()
-//            )
-
         println("Hello world, $config")
     }
 }
